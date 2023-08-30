2db28ba6eb2bc8ccec3de233a5b97d121783b09f
Sebatora <sebastiana.toranzo@gmail.com>
2023-08-30 15:11:38 -0300

    component user - navbar

--- app/page.js
+++ app/page.js
@@ -1,15 +1,16 @@
 import Users from '@/components/Users';
 import React from 'react'
 
-async function fetchUsers() {
-  const response = await fetch("https://reqres.in/api/users")
+async function getUsers() {
+  const response = await fetch(`https://reqres.in/api/users`)
   const { data } = await response.json();
-  console.log(data);
   return data;
 }
 
 async function HomePage() {
-  const users =  await fetchUsers();
+
+  const users =  await getUsers();
+
   return (
     <Users users={ users }/>
   )
--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
-function UserPage() {
+async function getUser (id) {
+const response = await fetch(`https://reqres.in/api/users/${id}`)
+const { data } = await response.json();
+return data;
+}
+
+async function UserPage( { params } ) {
+
+  const user =  await getUser(params.id);
+
   return (
-    <div>page</div>
+    <div className='bg-slate-400 p-10 rounded-md'>
+      <img src={user.avatar} alt={user.first_name} className='m-auto my-4' />
+      <h3 className='text-3xl font-bold'>{user.id} {user.first_name} {user.last_name}</h3>
+      <p>{user.email}</p>
+    </div>
   )
 }
 
